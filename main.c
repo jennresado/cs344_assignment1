5fde3b70e581f60ab46323f529800c955041aede
jennresado <doje@oregonstate.edu>
2021-01-10 20:45:32 -0800

    rating structure, interactive choice 2 and 3, formatting, file intake

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* 
+/*
 	Jennifer Do
 	Assignment 1: Movies
 	Reads a CSV file with movie data that is provided to the program
@@ -16,9 +16,9 @@
 
 /* struct for movie information */
 struct movie {
-	char *title;
+	char* title;
 	int year;
-	char *languages[5][21]; // 5 languages, 20 chars max + null
+	char* languages[5][21]; // 5 languages, 20 chars max + null
 	double ratingValue;
 	struct movie* next;
 };
@@ -27,7 +27,7 @@ struct movie {
 struct rating {
 	int year;
 	double ratingValue;
-	char *title;
+	char* title;
 	struct rating* next;
 };
 
@@ -35,15 +35,15 @@ struct rating {
 	Parse the current line which is comma delimited and create a
 	movie struct with the data in this line.
 */
-struct movie *createMovie(char *currLine) {
-	struct movie *currMovie = malloc(sizeof(struct movie));
+struct movie* createMovie(char* currLine) {
+	struct movie* currMovie = malloc(sizeof(struct movie));
 
 	// For use with strtok_r
-	char *saveptr1;
-	char *saveptr2;
+	char* saveptr1;
+	char* saveptr2;
 
 	// The first token is the title
-	char *token = strtok_r(currLine, ",", &saveptr1);
+	char* token = strtok_r(currLine, ",", &saveptr1);
 	currMovie->title = calloc(strlen(token) + 1, sizeof(char));
 	strcpy(currMovie->title, token);
 
@@ -53,18 +53,16 @@ struct movie *createMovie(char *currLine) {
 
 	// The next token is list of languages
 	token = strtok_r(NULL, ",", &saveptr1);
-	char *tokenLanguage = strtok_r(token, "[", &saveptr2);
-	tokenLanguage = strtok_r(NULL, ";", &saveptr2);
+	char* tokenLang;
 	int numLanguage = 0;
-	while (tokenLanguage != NULL) {
-		strcpy(currMovie->languages[numLanguage], tokenLanguage);
+	for (tokenLang = strtok_r(token, "[;]", &saveptr2); tokenLang; tokenLang = strtok_r(NULL, "[;]", &saveptr2)) {
+		strcpy(currMovie->languages[numLanguage], tokenLang);
 		numLanguage++;
-		tokenLanguage = strtok_r(NULL, ';]', &saveptr2);
 	}
 
 	// The last token is rating value
 	token = strtok_r(NULL, "\n", &saveptr1);
-	char *ptr;
+	char* ptr;
 	currMovie->ratingValue = strtod(token, ptr);
 
 	// Set the next node to NULL in the newly created student entry
@@ -76,8 +74,8 @@ struct movie *createMovie(char *currLine) {
 /*
 	Create a rating struct with current movie data.
 */
-struct rating* createRating(struct movie* aMovie) {
-	struct rating* currRating = malloc(sizeof(struct rating));
+struct rating *createRating(struct movie* aMovie) {
+	struct rating *currRating = malloc(sizeof(struct rating));
 
 	// Set the year
 	currRating->year = aMovie->year;
@@ -86,6 +84,7 @@ struct rating* createRating(struct movie* aMovie) {
 	currRating->ratingValue = aMovie->ratingValue;
 
 	// Title of movie of year
+	currRating->title = calloc(strlen(aMovie->title) + 1, sizeof(char));
 	strcpy(currRating->title, aMovie->title);
 
 	// Set the next node to NULL in newly created rating entry
@@ -98,54 +97,56 @@ struct rating* createRating(struct movie* aMovie) {
 	Return a linked list of movies by parsing data from
 	each line of the specified file.
 */
-struct movie *processFile(char* filePath) {
+struct movie *processFile(char *filePath) {
 	int movies = 0;
 
-	// Open the specified file for reading onlu
-	FILE *movieFile = fopen(filePath, "r");
+    // Open the specified file for reading only
+    FILE *movieFile = fopen(filePath, "r");
 
-	char *currLine = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	char *token;
+    char *currLine = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char *token;
 
-	// The head of the movie linked list
-	struct movie *head = NULL;
-	// The tail of the movie linked list
-	struct movie *tail = NULL;
+    // The head of the linked list
+    struct movie *head = NULL;
+    // The tail of the linked list
+    struct movie *tail = NULL;
 
-	// Read the file line by line
-	while ((nread = getLine(&currLine, &len, movieFile)) != -1) {
-		// Get a new movie node corresponding to the current line
-		struct movie *newNode = createMovie(currLine);
+	// Skip header
+	nread = getline(&currLine, &len, movieFile);
+
+    // Read the file line by line
+    while ((nread = getline(&currLine, &len, movieFile)) != -1) {
+        // Get a new student node corresponding to the current line
+        struct movie *newNode = createMovie(currLine);
 		movies++;
 
-		// Is this the first node in the linked list?
-		if (head == NULL) {
-			// This is the first node in the linked list
-			// Set the head and the tail to this node
-			head = newNode;
-			tail = newNode;
-		}
-		else {
-			// This is not the first node
-			// Add this node to the list and advance the tail
-			tail->next = newNode;
-			tail = newNode;
-		}
-	}
-	free(currLine);
-	fclose(movieFile);
+        // Is this the first node in the linked list?
+        if (head == NULL) {
+            // This is the first node in the linked link
+            // Set the head and the tail to this node
+            head = newNode;
+            tail = newNode;
+        } else {
+            // This is not the first node.
+            // Add this node to the list and advance the tail
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    free(currLine);
+    fclose(movieFile);
 
 	printf("Processed file %s and parsed data for %d movies\n", filePath, movies);
 
-	return head;
+    return head;
 }
 
 /*
 	Print movies released in a specified year
 */
-void printMoviesReleased(struct movie *list) {
+void printMoviesReleased(struct movie* list) {
 	int year;
 	int count = 0;
 
@@ -165,14 +166,14 @@ void printMoviesReleased(struct movie *list) {
 
 	// No movies were released in specified year
 	if (count == 0) {
-		printf("No data about movies released in the year %d", year);
+		printf("No data about movies released in the year %d\n", year);
 	}
 }
 
 /*
 	Print highest rated movies for each year
 */
-void printHighestRatedMoviesYr(struct movie *list) {
+void printHighestRatedMoviesYr(struct movie* list) {
 	// Find highest rated movie per year
 	struct rating* rList = processRatings(list);
 
@@ -196,6 +197,7 @@ int processRatings(struct movie* list) {
 		if (head == NULL) {
 			// Get a new rating node corresponding to the current movie
 			struct rating* newNode = createRating(list);
+
 			head = newNode;
 			tail = newNode;
 		}
@@ -215,6 +217,7 @@ int processRatings(struct movie* list) {
 				tail = newNode;
 			}
 		}
+		list = list->next;
 	}
 	return head;
 }
@@ -242,7 +245,7 @@ int checkRatingYear(struct rating* list, struct movie* aMovie) {
 /*
 	Print movies and their year of release for a specified language
 */
-void printMoviesSpecLang(struct movie *list) {
+void printMoviesSpecLang(struct movie* list) {
 	char language[21];
 	int count = 0;
 
@@ -253,8 +256,8 @@ void printMoviesSpecLang(struct movie *list) {
 	// for specified language
 	while (list != NULL) {
 		for (int i = 0; i < 5; i++) {
-			if (strcmp(language, list->languages[i])) {
-				printf("%d %s\n", list->year, list->languages[i]);
+			if (strcmp(language, list->languages[i]) == 0) {
+				printf("%d %s\n", list->year, list->title);
 				count++;
 			}
 		}
@@ -264,7 +267,7 @@ void printMoviesSpecLang(struct movie *list) {
 
 	// No movies were released for specified language
 	if (count == 0) {
-		printf("No data about movies released in %s", language);
+		printf("No data about movies released in %s\n", language);
 	}
 }
 
@@ -272,15 +275,17 @@ void printMoviesSpecLang(struct movie *list) {
 	Print menu of interactive choices to user and process
 	the selected choice.
 */
-void interactions(struct movie *list) {
+void interactions(struct movie* list) {
 	int choice;
+	char *choices = "\n"
+		"1. Show movies released in the specified year\n"
+		"2. Show highest rated movie for each year\n"
+		"3. Show the title and year of release of all movies in a specific language\n"
+		"4. Exit from the program\n"
+		"\n";
 
-	printf("1. Show movies released in the specified year\n");
-	printf("2. Show highest rated movie for each year\n");
-	printf("3. Show the title and year of release of all movies in a specific language\n");
-	printf("4. Exit from the program\n");
-	
 	// Receive input from user and process based on value
+	printf("%s", choices);
 	printf("Enter a choice from 1 to 4: ");
 	scanf("%d", &choice);
 
@@ -296,10 +301,12 @@ void interactions(struct movie *list) {
 			printMoviesSpecLang(list);
 			break;
 		default:
-			printf("You entere an incorrect choice. Try again.\n");
+			printf("You entered an incorrect choice. Try again.\n");
 			break;
 		}
 
+		// Receive input from user and process based on value
+		printf("%s", choices);
 		printf("Enter a choice from 1 to 4: ");
 		scanf("%d", &choice);
 	}
@@ -312,13 +319,13 @@ void interactions(struct movie *list) {
 	Compile the program as follows:
 		gcc --std=gnu99 -o movies main.c
 */
-int main(int argc, char* argv[]) {
-	if (argc < 2) {
-		printf("You must prove the name of the file to process\n");
-		printf("Example usage: ./movies movie_sample_1.csv\n");
-		return EXIT_FAILURE;
-	}
-	struct movie* list = processFile(argv[1]);
-	interactions(list);
-	return EXIT_SUCCESS;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("You must provide the name of the file to process\n");
+        printf("Example usage: ./movies movie_sample_1.txt\n");
+        return EXIT_FAILURE;
+    }
+    struct movie *list = processFile(argv[1]);
+    interactions(list);
+    return EXIT_SUCCESS;
 }
